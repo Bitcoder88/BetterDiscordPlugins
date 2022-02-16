cf3a90e8ed430423965b0df256450b178c97a8b2
Bitcoder88 <68890693+Bitcoder88@users.noreply.github.com>
2022-02-17 03:58:56 +0700

    Update MemeSounds.plugin.js

--- MemeSounds/MemeSounds.plugin.js
+++ MemeSounds/MemeSounds.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name MemeSounds
- * @version 0.5.9
+ * @version 0.5.9 (Bitcoder's Experimental Version)
  * @description Plays Memetastic sounds depending on what is being sent in chat. This was heavily inspired by the idea of Metalloriff's bruh plugin so go check him out!
  * @invite YMqKjWEVxG
  * @author Lonk#6942
@@ -36,6 +36,22 @@ module.exports = (() => {
 				{re: /bazinga/gmi, file: "bazinga.mp3", duration: 550},
 				{re: /oof/gmi, file: "oof.mp3", duration: 250},
 				{re: /bruh/gmi, file: "bruh.mp3", duration: 470}
+				{re: /dies/gmi, file: "MGS death.mp3", duration: 470}
+				{re: /chinese/gmi, file: "China Old Theme Meme", duration: 470}
+				{re: /hallelujah/gmi, file: "Hallelujah.mp3", duration: 470}
+				{re: /moans/gmi, file: "Yamete Kudasai.mp3", duration: 470}
+				{re: /silence/gmi, file: "Wind.mp3", duration: 470}
+				{re: /hell No/gmi, file: "Hell No WTF man.mp3", duration: 600},
+				{re: /japanese/gmi, file: "Japanese Yoo.mp3", duration: 550},
+				{re: /jumpscare/gmi, file: "Scary Jumpscare Fx.mp3", duration: 250},
+				{re: /trollge/gmi, file: "SUS dark fx..mp3", duration: 470}	 
+				{re: /among us/gmi, file: "AMONG US.mp3", duration: 470}
+						 
+				{re: /chinese/gmi, file: "China Old Theme Meme", duration: 470}
+				{re: /Hallelujah/gmi, file: "Hallelujah.mp3", duration: 470}
+				{re: /Moans/gmi, file: "Yamete Kudasai.mp3", duration: 470}
+				{re: /Silence/gmi, file: "Wind.mp3", duration: 470}
+				
 			];
 
 			/* Double message event fix */
